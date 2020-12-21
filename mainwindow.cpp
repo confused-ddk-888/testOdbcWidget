de013750fbbbe088edb72b9608f794827c257e63
confused-ddk888 <863654183@qq.com>
2020-12-21 11:37:33 +0800

    所有提示支持UTF，防止乱码出现。

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -4,6 +4,7 @@
 #include <QMessageBox>
 #include <QSqlError>
 #include <QDesktopWidget>
+#include "unicodeSupport.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -48,8 +49,8 @@ void MainWindow::ClickOnConnect()
 
     db = QSqlDatabase::addDatabase("QODBC");
 
-    ui->tbLog->append("支持的驱动：" + db.drivers().join(" "));
-    ui->tbLog->append(QString::fromUtf16(u"开始连接数据库....."));
+    ui->tbLog->append(QT_ODBC_UTFC_("支持的驱动：").append(db.drivers().join(" ")));
+    ui->tbLog->append(QT_ODBC_UTFC_("开始连接数据库....."));
 
     db.setDatabaseName(ui->tbDSN->toPlainText());
 
@@ -59,57 +60,55 @@ void MainWindow::ClickOnConnect()
 
         ui->tbLog->append(db.lastError().text());
 
-        ui->tbLog->append("Can't Connect to DB !");
-
-        //qDebug() << "Can't Connect to DB !";
+        ui->tbLog->append(QT_ODBC_UTFC_("不能连接数据库！"));
     }
     else
     {
-        ui->tbLog->append("连接数据库成功！");
+        ui->tbLog->append(QT_ODBC_UTFC_("连接数据库成功！"));
         //qDebug() << "Connected Successfully to DB !";
 
-        ui->tbLog->append("创建customer_t1....");
+        ui->tbLog->append(QT_ODBC_UTFC_("创建customer_t1...."));
 
         QSqlQuery query;
         query.prepare("drop table IF EXISTS customer_t1");
         if (!query.exec())
         {
-            ui->tbLog->append("创建失败 step 1.." + query.lastError().text());
+            ui->tbLog->append(QT_ODBC_UTFC_("创建失败 step 1..") + query.lastError().text());
         }
         else
         {
-            ui->tbLog->append("删除 customer_t1.... 成功.");
+            ui->tbLog->append(QT_ODBC_UTFC_("删除 customer_t1.... 成功."));
 
             query.clear();
 
             query.prepare("CREATE TABLE customer_t1(id INTEGER, name VARCHAR(32));");
             if(!query.exec())
             {
-                ui->tbLog->append("创建失败 step 2.." + query.lastError().text());
+                ui->tbLog->append(QT_ODBC_UTFC_("创建失败 step 2..") + query.lastError().text());
             }
             else
             {
-                ui->tbLog->append("创建 customer_t1.... 成功.");
+                ui->tbLog->append(QT_ODBC_UTFC_("创建 customer_t1.... 成功."));
 
                 query.clear();
                 query.prepare("insert into customer_t1(id, name) values(1, 'ddk')");
                 if (!query.exec())
                 {
-                    ui->tbLog->append("插入数据.." + query.lastError().text());
+                    ui->tbLog->append(QT_ODBC_UTFC_("插入数据..") + query.lastError().text());
                 }
                 else
                 {
-                    ui->tbLog->append("插入 customer_t1.... 成功.");
+                    ui->tbLog->append(QT_ODBC_UTFC_("插入 customer_t1.... 成功."));
 
                     query.clear();
                     query.prepare("SELECT * FROM customer_t1 WHERE ID = 1");
                     if(!query.exec())
                     {
-                        ui->tbLog->append("读取数据.." + query.lastError().text());
+                        ui->tbLog->append(QT_ODBC_UTFC_("读取数据..") + query.lastError().text());
                     }
                     else
                     {
-                        ui->tbLog->append("读取 customer_t1.... 成功.");
+                        ui->tbLog->append(QT_ODBC_UTFC_("读取 customer_t1.... 成功."));
                         while(query.next())
                         {
                             ui->tbLog->append("col 1:" + query.value(0).toString());
@@ -133,24 +132,24 @@ void MainWindow::ClickOnExec()
 {
     if (ui->tbDSN->toPlainText().length() == 0)
     {
-        QMessageBox::warning(this, "warning", "DSN is NULL, please input dsn string!");
+        QMessageBox::warning(this, QT_ODBC_UTFC_("错误"), QT_ODBC_UTFC_("数据源内容为空，请先输入数据源名称！"));
 
-        ui->tbLog->append("DSN is NULL, please input dsn string!");
+        ui->tbLog->append(QT_ODBC_UTFC_("数据源内容为空，请先输入数据源名称！"));
         return;
     }
 
     if (ui->tbSQL->toPlainText().length() == 0)
     {
-        QMessageBox::warning(this, "Error", "Please input Sql statement!");
+        QMessageBox::warning(this, QT_ODBC_UTFC_("错误"), QT_ODBC_UTFC_("SQL语句内容为空，请先输入SQL语句!"));
 
-        ui->tbLog->append("SQL is NULL, Please input Sql statement!");
+        ui->tbLog->append(QT_ODBC_UTFC_("SQL语句内容为空，请先输入SQL语句!"));
         return;
     }
 
     db = QSqlDatabase::addDatabase("QODBC");
 
-    ui->tbLog->append("支持的驱动：" + db.drivers().join(" "));
-    ui->tbLog->append("开始连接数据库.....");
+    ui->tbLog->append(QT_ODBC_UTFC_("支持的驱动：") + db.drivers().join(" "));
+    ui->tbLog->append(QT_ODBC_UTFC_("开始连接数据库....."));
 
     db.setDatabaseName(ui->tbDSN->toPlainText());
 
@@ -160,25 +159,25 @@ void MainWindow::ClickOnExec()
 
         ui->tbLog->append(db.lastError().text());
 
-        ui->tbLog->append("Can't Connect to DB !");
+        ui->tbLog->append(QT_ODBC_UTFC_("Can't Connect to DB !"));
 
         //qDebug() << "Can't Connect to DB !";
     }
     else
     {
-        ui->tbLog->append("连接数据库成功！");
+        ui->tbLog->append(QT_ODBC_UTFC_("连接数据库成功！"));
 
-        ui->tbLog->append("开始执行SQL....");
+        ui->tbLog->append(QT_ODBC_UTFC_("开始执行SQL...."));
 
         QSqlQuery query;
         query.prepare(ui->tbSQL->toPlainText());
         if (!query.exec())
         {
-            ui->tbLog->append("执行 SQL 失败:" + query.lastError().text());
+            ui->tbLog->append(QT_ODBC_UTFC_("执行 SQL 失败:") + query.lastError().text());
         }
         else
         {
-            ui->tbLog->append("执行 SQL 成功.");
+            ui->tbLog->append(QT_ODBC_UTFC_("执行 SQL 成功."));
         }
     }
     db.close();
--- unicodeSupport.h
+++ unicodeSupport.h
@@ -1,4 +1,8 @@
 #ifndef UNICODESUPPORT_H
 #define UNICODESUPPORT_H
 
+#include <QString>
+
+#define QT_ODBC_UTFC_(x) QString::fromLocal8Bit(x)
+
 #endif // UNICODESUPPORT_H
